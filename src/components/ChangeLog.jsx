8cffb2cae805abf732d2b99b51ae6dcb9cfcfc26
proxyGrit <dev@proxygrit.com>
2025-08-26 18:57:05 -0700

    create base ChangeLog component

---
+++ src/components/ChangeLog.jsx
@@ -0,0 +1,8 @@
+export default function ChangeLog() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>User Change Log</h2>
+      <ul></ul>
+    </div>
+  );
+}
--- src/components/TreeWrapper.jsx
+++ src/components/TreeWrapper.jsx
@@ -1,11 +1,15 @@
 import styles from './TreeWrapper.module.css';
 import Tree from './Tree';
+import ChangeLog from './ChangeLog';
 
 export default function TreeWrapper({ name, setName }) {
   return (
     <div className={styles['tree-wrapper']}>
       <div className={styles['tree-left']}>
         <Tree name={name} setName={setName} />
+      </div>
+      <div className={styles['tree-right']}>
+        <ChangeLog />
       </div>
     </div>
   );
